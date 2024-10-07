8900a53561dc8a8576c18f738ad74e346953cdaf
WajdButterfly <wajd2828@gmail.com>
2024-10-07 19:32:24 +0300

    s

--- CatCare/ContentView.swift
+++ CatCare/ContentView.swift
@@ -1,21 +1,36 @@
-//
-//  ContentView.swift
-//  CatCare
-//
-//  Created by maya alasiri  on 04/04/1446 AH.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isActive = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if self.isActive {
+            ZStack {
+                Color(hex: "FDB573")
+                    .ignoresSafeArea()
+//                Color(red: 1.0, green: 0.8, blue: 0.6)// Set the background color to orange
+                
+                Image("Kitty")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                
+                Text("CatCare")
+                .font(.title) // Set the font size
+                .foregroundColor(.black) // Set the text color
+                .padding(.top, 100) // Add some spacing above the text
+            }
+            
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    withAnimation {
+                        self.isActive = false
+                    }
+                }
+            }
+        } else {
+            ChooseCat()
         }
-        .padding()
     }
 }
 

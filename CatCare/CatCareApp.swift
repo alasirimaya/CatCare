1fb8d2d5af2d3967a66c958a77a5957f99a8b691
alasirimaya <alasirimaya@gmail.com>
2024-10-07 15:56:30 +0300

    Initial Commit

---
+++ CatCare/CatCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatCareApp.swift
+//  CatCare
+//
+//  Created by maya alasiri  on 04/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct CatCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
